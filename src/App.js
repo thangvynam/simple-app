8a2bb65c9af2719ebf0a3d36218296e6498c7863
thangvynam1808@gmail.com <Namdeptrai12#>
2023-01-08 09:52:21 +0700

    feat: change name

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello Nam Thang 2
+          Hello Nam Thang 3
         </p>
         <a
           className="App-link"
