97496e5eec1da05ed9f847cf70c4cd84f2cb1914
thangvynam1808@gmail.com <Namdeptrai12#>
2023-01-09 09:07:03 +0700

    feat: add stepper

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import SwipeableTextMobileStepper from './Stepper';
 
 function App() {
   return (
-    <div className="App">
+    
+    <div classN
+    ame="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Hello Nam Thang 3
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        {SwipeableTextMobileStepper()}
       </header>
     </div>
   );
